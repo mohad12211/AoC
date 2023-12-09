fb0a87e8eb6ff21059367df738790dcc3b18b4b7
mohad12211 <mohammedkaabi64@gmail.com>
2023-12-09 09:27:48 +0300

    2023 9

---
+++ AoC2023/day9/src/bin/part1.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // let input = include_str!("input.test.txt");
+    let input = include_str!("input.txt");
+    let sum: i64 = input
+        .lines()
+        .map(|line| {
+            let mut sequances = Vec::new();
+            let original: Vec<i64> = line.split(' ').map(|c| c.parse().unwrap()).collect();
+            let mut seq = original;
+            while seq.iter().any(|n| *n != seq[0]) {
+                sequances.push(seq);
+                seq = sequances
+                    .last()
+                    .unwrap()
+                    .windows(2)
+                    .map(|pair| pair[1] - pair[0])
+                    .collect();
+            }
+            let mut last = seq[0];
+            for seq in sequances.iter().rev() {
+                last = seq.last().unwrap() + last;
+            }
+            last
+        })
+        .sum();
+    println!("{sum}");
+}
