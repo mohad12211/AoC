81346277e50c29d3578c47cd9cfeaadd492e16e5
mohad12211 <mohammedkaabi64@gmail.com>
2023-12-14 00:43:58 +0300

    2017 1

---
+++ AoC2017/day01/src/bin/part2.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let nums: Vec<_> = input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    let mut sum = 0;
+    for (i, &num) in nums.iter().enumerate() {
+        if nums[(i + nums.len() / 2) % nums.len()] == num {
+            sum += num;
+        }
+    }
+
+    println!("{sum}");
+}
