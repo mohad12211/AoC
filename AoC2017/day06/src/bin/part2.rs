5a50cf7ca4219a427f123c22d6bfc5272c9951a1
mohad12211 <mohammedkaabi64@gmail.com>
2023-12-14 20:31:42 +0300

    2017 6

---
+++ AoC2017/day06/src/bin/part2.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+fn main() {
+    // let input = include_str!("input.test.txt");
+    let input = include_str!("input.txt");
+    let banks: Vec<usize> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let banks_count = banks.len();
+
+    let mut set = HashSet::new();
+    let mut current = banks.clone();
+    while !set.contains(&current) {
+        set.insert(current.clone());
+        let (index, max) = find_max(&current);
+        current[index] = 0;
+        for i in 1..=max {
+            current[(index + i) % banks_count] += 1;
+        }
+    }
+
+    let loop_start = current.clone();
+    let mut period = 0;
+    while current != loop_start || period == 0 {
+        period += 1;
+        let (index, max) = find_max(&current);
+        current[index] = 0;
+        for i in 1..=max {
+            current[(index + i) % banks_count] += 1;
+        }
+    }
+
+    println!("{period}");
+}
+
+fn find_max(banks: &Vec<usize>) -> (usize, usize) {
+    let max = banks.iter().max().unwrap();
+    let i = banks.iter().position(|x| x == max).unwrap();
+    (i, *max)
+}
