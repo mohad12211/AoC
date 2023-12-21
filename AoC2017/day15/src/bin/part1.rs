638b6f903823a84dbd2ab2f28dc01468e0e4128d
mohad12211 <mohammedkaabi64@gmail.com>
2023-12-21 20:50:22 +0300

    2017 15

---
+++ AoC2017/day15/src/bin/part1.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // let input = include_str!("input.test.txt");
+    let input = include_str!("input.txt");
+
+    let factors: [u64; 2] = [16807, 48271];
+    let mut previous: Vec<u64> = input
+        .lines()
+        .map(|l| l.rsplit_once(' ').unwrap().1.parse().unwrap())
+        .collect();
+
+    let mut match_count = 0;
+    for _ in 0..40_000_000 {
+        previous[0] = (previous[0] * factors[0]) % 2147483647;
+        previous[1] = (previous[1] * factors[1]) % 2147483647;
+        if match16bit(previous[0], previous[1]) {
+            match_count += 1;
+        }
+    }
+
+    println!("{match_count}");
+}
+
+fn match16bit(a: u64, b: u64) -> bool {
+    let mask = !(u64::MAX << 16);
+
+    (a & mask) == (b & mask)
+}
