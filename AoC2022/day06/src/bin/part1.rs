9815b3ded7b73cb1b32803d1f6324a5a44c5aa63
mohad12211 <mohammedkaabi64@gmail.com>
2024-12-02 14:06:46 +0300

    2022 06

---
+++ AoC2022/day06/src/bin/part1.rs
@@ -0,0 +1,14 @@
+use std::collections::HashSet;
+
+fn are_elements_unique(slice: &[u8]) -> bool {
+    let mut seen = HashSet::new();
+    slice.iter().all(|item| seen.insert(item))
+}
+
+fn main() {
+    // let input = include_str!("input.test.txt");
+    let input = include_str!("input.txt");
+
+    let result = input.as_bytes().windows(4).position(are_elements_unique);
+    println!("{}", result.unwrap() + 4);
+}
