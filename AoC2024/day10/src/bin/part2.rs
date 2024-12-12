5913c092dd042bbd35e4cb3574c47ab8b4677c37
mohad12211 <mohammedkaabi64@gmail.com>
2024-12-13 00:18:01 +0300

    2024 10

---
+++ AoC2024/day10/src/bin/part2.rs
@@ -0,0 +1,53 @@
+use std::collections::VecDeque;
+
+const DIRS: [(isize, isize); 4] = [(0, 1), (0, -1), (-1, 0), (1, 0)];
+
+fn main() {
+    // let input = include_str!("input.test.txt");
+    let input = include_str!("input.txt");
+
+    let grid: Vec<Vec<_>> = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect()
+        })
+        .collect();
+
+    let mut result = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] == 0 {
+                result += bfs(&grid, (x, y));
+            }
+        }
+    }
+    println!("{result}");
+}
+
+fn bfs(grid: &[Vec<usize>], start: (usize, usize)) -> usize {
+    let mut count = 0;
+
+    let mut queue = VecDeque::new();
+    queue.push_front(start);
+
+    while let Some((x, y)) = queue.pop_front() {
+        if grid[y][x] == 9 {
+            count += 1;
+            continue;
+        }
+        for (dx, dy) in DIRS {
+            let (nx, ny) = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy));
+            if grid
+                .get(ny)
+                .and_then(|row| row.get(nx))
+                .is_some_and(|&next_point| next_point == grid[y][x] + 1)
+            {
+                queue.push_front((nx, ny));
+            }
+        }
+    }
+
+    count
+}
